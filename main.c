9c01775a6aa607874ca6625588cd3b6a83c00007
CaPiM <61054205+Edson-br@users.noreply.github.com>
2022-06-16 23:34:08 -0300

    Troca da pilha para a fila

--- main.c
+++ main.c
@@ -15,7 +15,86 @@ typedef struct carta{
     Valor c_Valor;
 } Carta;
 
-typedef struct pilha {
+typedef struct fila {
+    Carta *inicio;
+    Carta *fim;
+} Fila;
+
+
+void colocaDadoFila(int n, int v, Fila *f) {
+    Carta *novoDado;
+    novoDado = (Carta *)malloc(sizeof(Carta));
+    novoDado->c_Naipe = n;
+    novoDado->c_Valor = v;
+    novoDado->prox = NULL;
+    if (f->inicio == NULL && f->fim == NULL)
+    {
+        f->fim = f->inicio = novoDado;
+    }
+    else
+    {
+        f->fim->prox = novoDado;
+        f->fim = novoDado;
+    }
+}
+
+Fila *inicializaFila() {
+    Fila *f;
+    f = (Fila *)malloc(sizeof(Fila));
+    f->inicio = NULL;
+    f->fim = NULL;
+    for(int i=0;i<13;i++){
+        for(int j=0;j<4;j++){
+            colocaDadoFila(j,i,f);
+        }
+    }
+    return f;
+}
+
+void imprimeFila(Fila *f) {
+    Carta *iterador;    
+    iterador = f->inicio;
+    while (iterador != NULL) {
+        switch(iterador->c_Naipe) {
+            case Copas:
+              printf("♥");
+              break;
+            case Ouros:
+              printf("♦");
+              break;
+            case Paus:
+              printf("♣");
+              break;
+            case Espadas:
+              printf("♠");
+              break;
+        }
+        if(iterador->c_Valor<Dama){
+            printf("%i",iterador->c_Valor+2);
+        }else{
+            switch(iterador->c_Valor) {
+                case Valete:
+                    printf("J");
+                    break;
+                case Dama:
+                    printf("Q");
+                    break;
+                case Rei:
+                    printf("K");
+                    break;
+                case As:
+                    printf("A");
+                    break;
+            }
+        }
+        printf("\n");
+        iterador = iterador->prox;
+    }
+	printf("  ");
+}
+
+//aqui estava o codigo para aplicao em pilha, mas o inicializaPilha esta pra fazer um baralho novo
+/*typedef struct pilha {
     Carta *topo;
 } Pilha;
 
@@ -80,10 +159,10 @@ void imprimePilha(Pilha *p) {
         iterador = iterador->prox;
     }
 	printf("  ");
-}
+}*/
 
 int main()
 {
-    Pilha *p = inicializaPilha();
-    imprimePilha(p);
+    Fila *f = inicializaFila();
+    imprimeFila(f);
 }
